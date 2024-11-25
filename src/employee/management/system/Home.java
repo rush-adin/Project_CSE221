01c1340a0fad969fd8da3deee4bd3b37118e95db
rush-adin <fardin23105101353@diu.edu.bd>
2024-11-25 14:41:02 +0600

    Login Panel

---
+++ src/employee/management/system/Home.java
@@ -0,0 +1,20 @@
+package employee.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Home extends JFrame implements ActionListener{
+
+    JButton view, add, update, remove;
+    
+    Home() {
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+    }
+
+    public static void main(String[] args) {
+        new Home();
+    }
+}

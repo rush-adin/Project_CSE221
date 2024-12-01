126ff330206a8876a21c12f988fb308a64ba4173
abirhasan11 <hasan23105101162@diu.edu.bd>
2024-12-01 19:54:35 +0600

    added update employee class

---
+++ src/employee/management/system/UpdateEmployee.java
@@ -0,0 +1,13 @@
+package employee.management.system;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class UpdateEmployee extends JFrame implements ActionListener{
+
+    JTextField tfeducation, tffname, tfaddress, tfphone, tfaadhar, tfemail, tfsalary, tfdesignation;
+    JLabel lblempId;
+    JButton add, back;
+    String empId;

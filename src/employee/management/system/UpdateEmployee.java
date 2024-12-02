0721c8a182eea2d26ebadef80bb75935af8bf81b
abirhasan11 <hasan23105101162@diu.edu.bd>
2024-12-02 14:48:38 +0600

    added fathers name , date of birth and  salary details

--- src/employee/management/system/UpdateEmployee.java
+++ src/employee/management/system/UpdateEmployee.java
@@ -30,3 +30,30 @@ public class UpdateEmployee extends JFrame implements ActionListener{
         JLabel lblname = new JLabel();
         lblname.setBounds(200, 150, 150, 30);
         add(lblname);
+
+        JLabel labelfname = new JLabel("Father's Name");
+        labelfname.setBounds(400, 150, 150, 30);
+        labelfname.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labelfname);
+
+        tffname = new JTextField();
+        tffname.setBounds(600, 150, 150, 30);
+        add(tffname);
+
+        JLabel labeldob = new JLabel("Date of Birth");
+        labeldob.setBounds(50, 200, 150, 30);
+        labeldob.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labeldob);
+
+        JLabel lbldob = new JLabel();
+        lbldob.setBounds(200, 200, 150, 30);
+        add(lbldob);
+
+        JLabel labelsalary = new JLabel("Salary");
+        labelsalary.setBounds(400, 200, 150, 30);
+        labelsalary.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labelsalary);
+
+        tfsalary = new JTextField();
+        tfsalary.setBounds(600, 200, 150, 30);
+        add(tfsalary);

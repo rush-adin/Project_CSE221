614b38b026c31902c060ab303ec50ab35cca1f50
abirhasan11 <hasan23105101162@diu.edu.bd>
2024-12-02 12:20:46 +0600

    added name and details

--- src/employee/management/system/UpdateEmployee.java
+++ src/employee/management/system/UpdateEmployee.java
@@ -16,3 +16,17 @@ public class UpdateEmployee extends JFrame implements ActionListener{
         this.empId = empId;
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
+
+        JLabel heading = new JLabel("Update Employee Detail");
+        heading.setBounds(320, 30, 500, 50);
+        heading.setFont(new Font("SAN_SERIF", Font.BOLD, 25));
+        add(heading);
+
+        JLabel labelname = new JLabel("Name");
+        labelname.setBounds(50, 150, 150, 30);
+        labelname.setFont(new Font("serif", Font.PLAIN, 20));
+        add(labelname);
+
+        JLabel lblname = new JLabel();
+        lblname.setBounds(200, 150, 150, 30);
+        add(lblname);
